b281df4bd47e221143f404d836bfffc6b02a2e9e
KwintonDropps <65418687+KwintonDropps@users.noreply.github.com>
2020-11-21 23:59:35 -0600

    Made some changes to the insert function in fifo.

--- FIFO.java
+++ FIFO.java
@@ -1,5 +1,7 @@
 public class FIFO extends ReplacementAlgorithm{
     int[] Frame;
+    int faultCount;
+   
 
     protected FIFO(int pageFrameCount) {
         super(pageFrameCount);
@@ -20,7 +22,30 @@ public class FIFO extends ReplacementAlgorithm{
     }
 
     @Override
-    public void insert(int pageNumber) {
-
-    }
+    public void insert(int pageNumber, int[] referenceString) {  
+        int referenceStringSize = referenceString.length;
+        int index = 0;
+        int referenceIndex = 0;
+        while(index < referenceStringSize){
+            if(Frame.length < pageFrameCount){
+                Frame[index] = referenceString[referenceIndex];
+                faultCount++;
+                referenceIndex++;
+                break;
+            }
+            boolean insertFlag = searchForPage(pageNumber); 
+            if(!insertFlag){
+                int indexToReplaceAt = index % pageFrameCount + 1; 
+                Frame[indexToReplaceAt] = referenceString[referenceIndex];
+                faultCount++;
+                referenceIndex++;
+                break;    
+            }
+            else{
+                index--;
+                referenceIndex++;
+                break;
+            }   
+            index++;
+        }
 }
--- Optimal.java
+++ Optimal.java
@@ -1,12 +1,29 @@
 public class Optimal extends ReplacementAlgorithm{
-    
+    int[] Frame;
+    int faultCount;
 
     protected Optimal(int pageFrameCount) {
         super(pageFrameCount);
     }
 
+    public boolean searchForPage(int pageNumber){
+        boolean pageFound = false;
+
+        for(int i = 0; i < Frame.length; i++ ){
+            if(pageNumber == Frame[i]){
+                pageFound = true;
+                break;
+            }
+        }
+    
+        return pageFound;
+
+    }
+
     @Override
     public void insert(int pageNumber) {
+    
+    
     }
     
 }
